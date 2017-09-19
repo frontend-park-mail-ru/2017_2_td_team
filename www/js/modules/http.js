ce816816d9995deaa7de475e8f6e306f4c122cc4
Oleg Utkin <oleg_utkin@me.com>
2017-09-19 19:36:16 +0300

    Add http module

--- www/js/modules/http.js
+++ www/js/modules/http.js
@@ -1,3 +1,28 @@
 export class Http {
+  static get(address) {
+    return fetch(address, {
+      method: 'get',
+      credential: 'include'
+    })
+      .then(Http.checkStatus)
+      .then(res => res.json());
+  }
 
+  static post(address, body) {
+    return fetch(address, {
+      method: 'post',
+      credential: 'include',
+      headers: {'Content-Type': 'application/json; charset=utf8'},
+      body: JSON.stringify(body)
+    })
+      .then(Http.checkStatus)
+      .then(res => res.json());
+  }
+
+  static checkStatus(res) {
+    if (res.status >= 400)
+      return Promise.resolve(res);
+    else
+      return Promise.reject(new Error(res.text()));
+  }
 }
