c9b7300023015cb74b4f7a4d3ee4c5d683e8e381
Oleg Utkin <oleg_utkin@me.com>
2017-09-20 12:03:42 +0300

    Fix and reformat http class

--- www/js/modules/http.js
+++ www/js/modules/http.js
@@ -1,28 +1,28 @@
 export class Http {
-  static get(address) {
-    return fetch(address, {
-      method: 'get',
-      credentials: 'include'
-    })
-      .then(Http.checkStatus)
-      .then(res => res.json());
-  }
+    static get(address) {
+        return fetch(address, {
+            method: 'get',
+            credentials: 'include'
+        })
+            .then(Http.checkStatus)
+            .then(res => res.json());
+    }
 
-  static post(address, body) {
-    return fetch(address, {
-      method: 'post',
-      credentials: 'include',
-      headers: {'Content-Type': 'application/json; charset=utf8'},
-      body: JSON.stringify(body)
-    })
-      .then(Http.checkStatus)
-      .then(res => res.json());
-  }
+    static post(address, body) {
+        return fetch(address, {
+            method: 'post',
+            credentials: 'include',
+            headers: {'Content-Type': 'application/json; charset=utf8'},
+            body: JSON.stringify(body)
+        })
+            .then(Http.checkStatus)
+            .then(res => res.json());
+    }
 
-  static checkStatus(res) {
-    if (res.status >= 400)
-      return Promise.resolve(res);
-    else
-      return Promise.reject(new Error(res.text()));
-  }
+    static checkStatus(res) {
+        if (res.status < 400)
+            return Promise.resolve(res);
+        else
+            return Promise.reject(new Error(res.text()));
+    }
 }
