eeb57f8094236fe6ec013a8e062826d0a036bac4
Dnnd <ddann9@live.com>
2017-09-28 02:07:22 +0300

    + logoSpec

---
+++ spec/blocks/logo.js
@@ -0,0 +1,23 @@
+import {describe} from '../helpers/jasmineES6';
+import {Logo} from '../../www/js/blocks/logo/index';
+
+describe('Logo', () => {
+    it('По-умолчанию создается пустой логотип', () => {
+        const logo = new Logo();
+        expect(logo.innerHTML).toBe('');
+        const node = injectBlockNodeAccessor(button).node;
+        expect(node.tagName.toLowerCase()).toBe('div');
+        expect(node.attributes.length).toBe(0);
+        expect(node.classList.length).toBe(0);
+    });
+
+    it('Конструктор создает логотип по описанию', () => {
+        const logo = new Logo('logo', {'name': 'somename'}, ['someclass']);
+        const node = injectBlockNodeAccessor(logo).node;
+        expect(node.classList.length).toBe(1);
+        expect(node.classList[0]).toBe('someclass');
+        expect(node.attributes.length).toBe(1);
+        expect(node.getAttribute('name')).toBe('somename');
+        expect(node.text).toBe('logo');
+    });
+});
