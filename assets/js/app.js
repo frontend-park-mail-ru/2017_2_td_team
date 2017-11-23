4df93eb83f75a8fce3de5f26a4a62315a6874c2e
Dnnd <ddann9@live.com>
2017-11-24 02:15:00 +0300

    sw +(I hope), textures +

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,3 +60,12 @@ UserService
             globalEventBus.emit('router:redirect', {path: '/signin'});
         }
     });
+
+navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    .then(function(reg) {
+    // регистрация сработала
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+}).catch(function(error) {
+    // регистрация прошла неудачно
+    console.log('Registration failed with ' + error);
+});
