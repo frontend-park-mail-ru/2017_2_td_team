79d5a125d6257b104ea450c620a2e1fa32bd2db9
Dnnd <ddann9@live.com>
2017-11-24 15:10:10 +0300

    fix sw

--- www/sw.js
+++ www/sw.js
@@ -1,28 +1,40 @@
 this.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                '/bundle.js',
-                '/styles.css',
-                '/img/textures/attackIcon.png',
-                '/img/textures/aspeedIcon.png',
-                '/img/textures/hpIcon.png',
-                '/img/textures/moneyIcon.png',
-                '/img/titleset.png',
-                '/img/titleset.json',
-                '/7daab5c03279911663a04626da53f397.woff',
-                '/1316bd7dab0eccb7c1ae1f7ba5b243c3.woff',
-                '/122068bcd0df5f7f97e2d82ed60d9c8b.woff',
-                '/e4a79871d104b1e9bada243a05cc78ed.woff',
-                '/ed92ab9e8761f7ef025dd0b5f198db35.woff',
-                '/index.html'
-            ]);
-        })
+        caches
+            .open('v1')
+            .then(cache => {
+                return cache.addAll([
+                    '/bundle.js',
+                    '/styles.css',
+                    '/img/textures/attackIcon.png',
+                    '/img/textures/aspeedIcon.png',
+                    '/img/textures/hpIcon.png',
+                    '/img/textures/bluetower.png',
+                    '/img/textures/otower.png',
+                    '/img/textures/redtower.png',
+                    '/img/textures/timerIcon.png',
+                    '/img/textures/moneyIcon.png',
+                    '/img/titleset.png',
+                    '/img/titleset.json',
+                    '/7daab5c03279911663a04626da53f397.woff',
+                    '/1316bd7dab0eccb7c1ae1f7ba5b243c3.woff',
+                    '/122068bcd0df5f7f97e2d82ed60d9c8b.woff',
+                    '/e4a79871d104b1e9bada243a05cc78ed.woff',
+                    '/ed92ab9e8761f7ef025dd0b5f198db35.woff',
+                    '/index.html',
+                    '/',
+                    '/game',
+                    '/about',
+                    '/signup',
+                    '/signin',
+                    '/settings',
+                ]);
+            })
     );
 });
 
 this.addEventListener('fetch', event => {
-    console.log('fetch', event);
+
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request)
