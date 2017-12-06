52174e596b3330cad793a10b65bd84aa43141298
Dnnd <ddann9@live.com>
2017-12-06 13:06:43 +0300

    fix sw

--- www/sw.js
+++ www/sw.js
@@ -1,47 +1,61 @@
+const CACHE_NAME = 'v1';
+
+const RESOURCES_TO_CACHE = [
+    '/bundle.js',
+    '/styles.css',
+    '/img/textures/attackIcon.png',
+    '/img/textures/aspeedIcon.png',
+    '/img/textures/hpIcon.png',
+    '/img/textures/bluetower.png',
+    '/img/textures/otower.png',
+    '/img/textures/redtower.png',
+    '/img/textures/timerIcon.png',
+    '/img/textures/moneyIcon.png',
+    '/img/titleset.png',
+    '/img/titleset.json',
+    '/7daab5c03279911663a04626da53f397.woff',
+    '/1316bd7dab0eccb7c1ae1f7ba5b243c3.woff',
+    '/122068bcd0df5f7f97e2d82ed60d9c8b.woff',
+    '/e4a79871d104b1e9bada243a05cc78ed.woff',
+    '/ed92ab9e8761f7ef025dd0b5f198db35.woff',
+    '/index.html',
+];
+
 this.addEventListener('install', event => {
     event.waitUntil(
         caches
-            .open('v1')
+            .open(CACHE_NAME)
             .then(cache => {
-                return cache.addAll([
-                    '/bundle.js',
-                    '/styles.css',
-                    '/img/textures/attackIcon.png',
-                    '/img/textures/aspeedIcon.png',
-                    '/img/textures/hpIcon.png',
-                    '/img/textures/bluetower.png',
-                    '/img/textures/otower.png',
-                    '/img/textures/redtower.png',
-                    '/img/textures/timerIcon.png',
-                    '/img/textures/moneyIcon.png',
-                    '/img/titleset.png',
-                    '/img/titleset.json',
-                    '/7daab5c03279911663a04626da53f397.woff',
-                    '/1316bd7dab0eccb7c1ae1f7ba5b243c3.woff',
-                    '/122068bcd0df5f7f97e2d82ed60d9c8b.woff',
-                    '/e4a79871d104b1e9bada243a05cc78ed.woff',
-                    '/ed92ab9e8761f7ef025dd0b5f198db35.woff',
-                    '/index.html',
-                    '/',
-                    '/game',
-                    '/about',
-                    '/signup',
-                    '/signin',
-                    '/settings',
-                    '/logout',
-                ]);
+                return cache.addAll(RESOURCES_TO_CACHE);
             })
     );
 });
 
-// this.addEventListener('fetch', event => {
-//
-//
-//     event.respondWith(
-//         () => fetch(event.request)
-//             .then(r => r)
-//             .catch(r => cache.match(event.request))
-//     );
-//
-// });
+this.addEventListener('fetch', event => {
+
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse && !navigator.onLine) {
+                    return cachedResponse;
+                }
+                const request = event.request.clone();
+                return fetch(request)
+                    .then(response => {
+                            if (!response || response.status >= 300 || response.type !== 'basic') {
+                                return response;
+                            }
+                            if (!cachedResponse) {
+                                const responseToCache = response.clone();
+                                caches
+                                    .open(CACHE_NAME)
+                                    .then(cache => cache.put(event.request, responseToCache));
+                            }
+                            return response;
+                        }
+                    );
+            })
+    );
+});
 
