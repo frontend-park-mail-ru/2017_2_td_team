60c4871f4947e68db0d4b7c910f64fd57eec20f2
oleggator <oleg@utkin.xyz>
2017-12-12 00:14:38 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+PORT=228
+DIST_PATH=./dist/www
+DEPLOY_PATH=/var/www/td
 SERVER_IP=$1
 echo "Deploying to $SERVER_IP"
 
+echo "Setting up ssh..."
 eval "$(ssh-agent -s)"
 ssh-keyscan -p228 -H $SERVER_IP >> ~/.ssh/known_hosts
 chmod 600 $HOME/.ssh/server
 ssh-add $HOME/.ssh/server
 
-ssh -p228 td@$SERVER_IP rm -rf /var/www/td/*
+echo "Build..."
+npm run build
+
+echo "Cleaning up..."
+# ssh -p228 td@$SERVER_IP rm -rf /var/www/td/*
+
+echo "Uploading..."
 scp -P228 -r ./dist/www td@$SERVER_IP:/var/www/td
