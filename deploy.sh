54e0ac0076d3d511e016e9bb99197f3c724aa05c
oleggator <oleg@utkin.xyz>
2017-12-11 23:40:05 +0300

    Setup deploy to vps

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SERVER_IP=$1
+echo "Deploying to $SERVER_IP"
+
+eval "$(ssh-agent -s)"
+ssh-keyscan -H $SERVER_IP >> ~/.ssh/known_hosts
+chmod 600 $HOME/.ssh/server
+ssh-add $HOME/.ssh/server
+
+ssh -p228 td@$SERVER_IP rm -rf /var/www/td/*
+scp -P228 -r ./dist/www td@$SERVER_IP:/var/www/td
