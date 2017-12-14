f662fee4fbd69ddd1029bca2e8e0693f2c344272
oleggator <oleg@utkin.xyz>
2017-12-14 16:40:57 +0300

    Change backend address

--- assets/js/configs/backend.js
+++ assets/js/configs/backend.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'td-java.herokuapp.com/';
+export const BACKEND_URL = 'tdgame.pw/';
 export const buildBackendUrl = (path) =>'https://' + BACKEND_URL.concat(path);
 export const buildWebsocketUrl = (path) => 'wss://' + BACKEND_URL.concat(path);
 
